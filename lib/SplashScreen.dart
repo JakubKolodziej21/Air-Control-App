75632d7dd4793711f615b20c808df016330e90ae
Jakub Kołodziej <kubikolodziej@gmail.com>
2024-03-22 09:15:45 +0100

    AddSomeFirstTexts

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -26,19 +26,38 @@ class _SplashScreenState extends State<SplashScreen> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(),
+      body: Stack(
+        fit: StackFit.expand,        
+        children: <Widget>[
+          Container(
+
+          ),
+          Align(
+            alignment: FractionalOffset.center,
+            child:
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.air, color: const Color.fromARGB(255, 11, 1, 1)),
+                Padding(padding: EdgeInsets.only(top: 15.0)),
+                Text(widget.title),
+                Padding(padding: EdgeInsets.only(top: 5.0)),
+                Text('Aplikacja do monitorowania \n czystości powietrza'),
+
+              ],
+            )
+
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 35,
+            
+            child: Container(
+            alignment: Alignment.center,
+            child: Text('Przedmuchuje dane...'),
+          ))
+        ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
