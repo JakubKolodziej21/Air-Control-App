6e0f8c8f45a0675f9286c617f375e72d74c3cfdc
Jakub Kołodziej <kubikolodziej@gmail.com>
2024-03-24 18:38:00 +0100

    FirstScreenReady

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key, required this.title});
@@ -35,7 +36,7 @@ class _SplashScreenState extends State<SplashScreen> {
               gradient: LinearGradient(
                 begin: Alignment.centerRight,
                 end: Alignment.centerLeft,
-                colors: [new Color.fromARGB(255, 8, 218, 225), Color.fromARGB(255, 255, 0, 242)]
+                colors: [Color(0xFF00F7FF), Color.fromARGB(255, 52, 133, 255)]
               )
              ),
 
@@ -46,11 +47,26 @@ class _SplashScreenState extends State<SplashScreen> {
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Icon(Icons.air, color: const Color.fromARGB(255, 11, 1, 1)),
+                Image(image: AssetImage('icons/cloud-sun.png')),
                 Padding(padding: EdgeInsets.only(top: 15.0)),
-                Text(widget.title),
+                Text(widget.title,
+                textAlign: TextAlign.center,
+                style: GoogleFonts.lato(
+                  textStyle: TextStyle(
+                    fontSize: 42.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ) 
+                ),),
                 Padding(padding: EdgeInsets.only(top: 5.0)),
-                Text('Aplikacja do monitorowania \n czystości powietrza'),
+                Text('Aplikacja do monitorowania \n czystości powietrza',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.lato(
+                  textStyle: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.white,
+                  ) 
+                ),),
 
               ],
             )
@@ -63,7 +79,15 @@ class _SplashScreenState extends State<SplashScreen> {
             
             child: Container(
             alignment: Alignment.center,
-            child: Text('Przedmuchuje dane...'),
+            child: Text('Przedmuchuje dane...',
+            textAlign: TextAlign.center,
+                style: GoogleFonts.lato(
+                  textStyle: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w300,
+                    color: Colors.white,
+                  ) 
+                ),),
           ))
         ],
       ), // This trailing comma makes auto-formatting nicer for build methods.
